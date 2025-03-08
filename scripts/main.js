a43d861c6e003b582cb1880a56d17734e14b243b
VictorLeung91 <victorleung91@gmail.com>
2025-03-08 00:34:13 -0700

    Re-upload again
    Created two html inside "html file" which are "footer.html" and "header.html". We can reuse the same header and foot by "id=header-mainjs" and "id=foot-mainjs". Making changes by once instaed of tons in the future. Ask me for more information if you dont understand.
    
    Moved index.html back to outside of "html file".

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,5 @@
-Somethigns
+//Script file for public used.
+$(document).ready(function(){
+    $("#header-mainjs").load("../html/header.html");
+    $("#footer-mainjs").load("../html/footer.html");
+});
