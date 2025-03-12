51d07772c5e52a1d3fc3d020725b1f7bf0143026
VictorLeung91 <victorleung91@gmail.com>
2025-03-12 12:17:49 -0600

    signup page

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,5 @@
 $(document).ready(function(){
     $("#header-mainjs").load("html/header.html");
     $("#footer-mainjs").load("html/footer.html");
+    $("#leftSideBar-mainjs").load("html/leftsidebar.html");
 });
