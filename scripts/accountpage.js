32874be621847e1d8a529bdbb0e6deaf1d770842
VictorLeung91 <victorleung91@gmail.com>
2025-03-15 23:51:26 -0600

    updated logout button

--- scripts/accountpage.js
+++ scripts/accountpage.js
@@ -1,30 +1,3 @@
-//This session is for the left side bar.
-$(document).ready(function () {
-    //Loading the letf side bar.
-    $("#leftSideBar-mainjs").load("leftsidebar.html", function () {
-
-        //Get elements
-        const sidebarButtons = $('.sidebar-btn');
-        const contentSections = $('.content-section');
-
-        //Click function on left side bar(Edit profile only so far)
-        sidebarButtons.on('click', function (e) {
-            e.preventDefault(); 
-
-            //Remove default active class for all button
-            sidebarButtons.removeClass('active');
-            //Add active class to the one user pointing
-            $(this).addClass('active');
-
-            contentSections.hide();
-
-            const sectionId = $(this).data('section');
-            $(`#${sectionId}`).show();
-        });
-
-        $('input[data-section="edit-profile"]').addClass('active');
-    });
-});
 
 //This session is for the "Edit Profile" page.
 $(document).ready(function() {
