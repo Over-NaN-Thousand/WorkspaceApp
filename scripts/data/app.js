c93b96eb26e28f1f914eda842c2070d2b5b036f5
VictorLeung91 <victorleung91@gmail.com>
2025-03-28 15:56:55 -0600

    Plain files

--- scripts/data/app.js
+++ scripts/data/app.js
@@ -1,27 +1,35 @@
-//Import application
+//Import application(For public)
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
-const {  MongoClient } = require('mongodb');
-const { 
-    connectToDatabase, 
-    getHighestId, 
-    createProperty, 
-    readProperties, 
-    updateProperty, 
-    deleteProperty 
+const { MongoClient } = require('mongodb');
+const {
+    connectToDatabase,
+    getHighestId,
+    createProperty,
+    readProperties,
+    updateProperty,
+    deleteProperty
 } = require('./mongo');//To import connectToDatabase from mongo.js
-
-//Declare the app, port number
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+//Import jsonwebtoken and crypto(For member)
+const jwt = require('jsonwebtoken');
+const crypto = require('node:crypto');
+
+//To get the secret key(64-byte, saltString)(For member)
+const salt = crypto.randomBytes(64);
+const saltString = salt.toString(`hex`);
+
+
 
 // app and settings
 app.use(cors()); // allow all requests.
 app.use(express.json()); // define middleware to parse json
 
-//Define the Database's name.
+//Define and connect the Database's name.
 const DATABASE = "WorkspaceApp";
 
 
@@ -34,9 +42,9 @@ const DATABASE = "WorkspaceApp";
 //==========================================Keep CRUD below=========================================//
 
 //For testing used
-app.get(`/test-db`, async (req, res)=>{
-    try{
-        await connectToDatabase(async (client)=>{  //Connect to database first
+app.get(`/test-db`, async (req, res) => {
+    try {
+        await connectToDatabase(async (client) => {  //Connect to database first
             const databasesList = await client.db().admin().listDatabases(); //Display all database files
             const db = client.db(DATABASE);
             const collections = await db.listCollections().toArray(); //Display all the collections in database
@@ -51,99 +59,21 @@ app.get(`/test-db`, async (req, res)=>{
         res.status(500).json({ message: 'Failed to connect to MongoDB', error: error.message });
     }
 });
-//==================================CRUD for Property===================================================//
-app.post("/properties", async (req, res) => {
-    const newProperty = req.body;
+//======================================================================================================//
 
-    if (!newProperty.propertyId || !newProperty.name || !newProperty.ownerId)  // make sure all required fields are provided
-        return res.status(400).json({ message: "Missing required fields: propertyId, name, or ownerId." });
+//==================================Routes for Property===================================================//
 
-    try {
-        const highestPropertyId = await connectToDatabase(getHighestId,"property","propertyId"); //get current highest propertyId, we'll add 1
-        newProperty.propertyId = (highestPropertyId?.propertyId || 0) + 1; // ? is the optional chaining operator, if highestPropertyId is null or undefined, it will return 0
+//==================================End of Routes for Property===================================================//
 
-        const result = await connectToDatabase(createProperty, newProperty);
-            if (result.acknowledged) {
-                res.status(201).json({ message: "Property created successfully.", property: newProperty });
-            } else {
-                res.status(500).json({ message: "Failed to create property." });
-            }
-    } catch (error) {
-        console.error("Error creating property:", error);
-        res.status(500).json({ message: "An error occurred while creating the property." });
-    }
-});
 
-app.get("/properties", async (req, res) => {
-    const filters = {};
 
-    const ownerId = Number(req.headers["userid"] || req.query.userid); // try to get userId from headers, after that from query string
-    if (!isNaN(ownerId)) {
-        filters.ownerId = ownerId; 
-    } else {
-        console.error("Invalid ownerId provided:", ownerId);
-        return res.status(400).json({ message: "Invalid ownerId format." });
-    }
+//==================================Routes for user==========================================================//
 
-    const propertyName = req.headers["name"] || req.query.name;
-    if (propertyName) 
-        filters.name = { $regex: req.query.name, $options: "i" }; // AL: MongoDB regex similar to LIKE, i for case-insensitive
 
-    try {
-        const properties = await connectToDatabase(readProperties, filters);
-        // console.dir(properties);
-        res.status(200).json({ properties });
-    } catch (error) {
-        console.error("Error fetching properties:", error);
-        res.status(500).json({ message: "An error occurred while fetching properties." });
-    }
-});
 
-app.put("/properties/:id", async (req, res) => {
-    const propertyId = Number(req.params.id);       // get the property ID
-    const updates = req.body;                       // get the updates
+//==================================End of Routes for user==========================================================//
 
-    if (isNaN(propertyId))
-        return res.status(400).json({ message: "Invalid property ID provided." });
-    
 
-    if (!updates || Object.keys(updates).length === 0) 
-        return res.status(400).json({ message: "No updates provided in request body." });
-    
-    try {
-        const result = await connectToDatabase(updateProperty, propertyId, updates);
-        if (result.modifiedCount > 0) {
-            res.status(200).json({ message: "Property updated successfully." });
-        } else {
-            res.status(404).json({ message: "Property not found or no changes were made." });
-        }
-    } catch (error) {
-        console.error("Error updating property:", error);
-        res.status(500).json({ message: "An error occurred while updating the property." });
-    }
-});
-
-app.delete("/properties/:id", async (req, res) => {
-    const propertyId = Number(req.params.id);
-
-    if (isNaN(propertyId)) {
-        return res.status(400).json({ message: "Invalid property ID provided." });
-    }
-
-    try {
-        const result = await connectToDatabase(deleteProperty, propertyId);
-        if (result.deletedCount > 0) {
-            res.status(200).json({ message: "Property deleted successfully." });
-        } else {
-            res.status(404).json({ message: "Property not found." });
-        }
-    } catch (error) {
-        console.error("Error deleting property:", error);
-        res.status(500).json({ message: "An error occurred while deleting the property." });
-    }
-});
-
-//==================================End of CRUD for Property===================================================//
 
 
 app.listen(PORT, () => {
--- scripts/data/mongo.js
+++ scripts/data/mongo.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
-
 const {  MongoClient, ObjectId } = require('mongodb');
 const DATABASE = "WorkspaceApp"//Define the Database's name.
+const crypto = require('crypto');
+let users = loadUsers();
 
 async function connectToDatabase(callback, ...args) {
     /****************Put this code into your .env*****************
@@ -26,95 +27,40 @@ async function connectToDatabase(callback, ...args) {
     }
 }
 
-//========================The functions of Property=======================//
 async function listDatabases(client){
     const databasesList = await client.db().admin().listDatabases();
     console.log('Databases:');
     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 }
 
+//========================The functions of Property=======================//
 
-async function getHighestId(client, collection, idField) {
-    try {
-        const result = await client
-            .db(DATABASE)
-            .collection(collection)
-            .find()
-            .sort({ [idField]: -1 })   // sort by propertyId DESC
-            .limit(1)                   // top 1
-            .next();
 
-        return result;
-    } catch (error) {
-        console.error("Error fetching the highest propertyId:", error);
-        throw error;
-    }
-}
 
-async function createProperty(client, property) {
-    try {
-        const result = await client
-            .db(DATABASE)
-            .collection("properties")
-            .insertOne(property); // insert the property
-        
-        console.log(`Property inserted successfully with ID: ${result.insertedId}`);
-        return result;
-    } catch (error) {
-        console.error("Error inserting property into the database:", error);
-        throw error;
-    }
-}
 
-async function readProperties(client, filters) {
-    try {
-        const properties = await client
-            .db(DATABASE)
-            .collection("properties")
-            .find(filters)
-            .toArray(); // make an array
 
-        console.log(`Retrieved ${properties.length} property(ies).`);
-        return properties;
-    } catch (error) {
-        console.error("Error retrieving properties:", error);
-        throw error;
-    }
-}
-
-async function updateProperty(client, propertyId, updates) {
-    try {
-        const result = await client
-            .db(DATABASE)
-            .collection("properties")
-            .updateOne(
-                { propertyId }, // only update one document with propertyId
-                { $set: updates }
-            );
-
-        console.log(`Modified property:  propertyId=${propertyId}.`);
-        return result;
-    } catch (error) {
-        console.error("Error updating property: ", error);
-        throw error;
-    }
-}
-
-async function deleteProperty(client, propertyId) {
-    try {
-        const result = await client
-            .db(DATABASE)
-            .collection("properties")
-            .deleteOne({ propertyId });
-        console.log(`Deleted property: propertyId=${propertyId}.`);
-        return result;
-    } catch (error) {
-        console.error("Error deleting property:", error);
-        throw error;
-    }
-}
 //===================End of the function of Property==========================//
 
+
+//===================The function of user===================================//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//===================End of the function of user==========================//
 module.exports = { 
     connectToDatabase, 
     ObjectId, 
