6214a78a6457985a4f3c6dab58e47de067bdb873
VictorLeung91 <victorleung91@gmail.com>
2025-03-29 17:04:43 -0600

    latest mongo.js

--- scripts/data/mongo.js
+++ scripts/data/mongo.js
@@ -2,9 +2,8 @@ require('dotenv').config();
 const {  MongoClient, ObjectId } = require('mongodb');
 const DATABASE = "WorkspaceApp"//Define the Database's name.
 const crypto = require('crypto');
-
 //let users = loadUsers();
-
+const jwt = require('jsonwebtoken');
 
 async function connectToDatabase(callback, ...args) {
     /****************Put this code into your .env*****************
@@ -13,8 +12,8 @@ async function connectToDatabase(callback, ...args) {
     const db_uri = process.env.MONGO_URI;
     const client = new MongoClient(db_uri);
 
-
 //=========Please notice everyone if you have edited above code=================//
+
 
     try {
         await client.connect();
@@ -22,11 +21,19 @@ async function connectToDatabase(callback, ...args) {
         await callback(client, ...args);
     } catch (e) {
         console.error(e);
-
+        
     } finally {
         await client.close();
         console.log('Disconnected from database\n');
     }
+}
+
+async function deleteById(collectionName, id) {
+    return await connectToDatabase(async (client) => {
+        const db = client.db(DATABASE);
+        const result = await db.collection(collectionName).deleteOne({ _id: new ObjectId(id) });
+        return result;
+    });
 }
 
 
@@ -39,13 +46,32 @@ async function listDatabases(client){
 //========================The functions of Property=======================//
 
 
-
-
-
 //===================End of the function of Property==========================//
 
 
 //===================The function of user===================================//
+function hashPassword(password, salt) {
+    // https://www.geeksforgeeks.org/node-js-crypto-pbkdf2sync-method/
+    return crypto.pbkdf2Sync(password, salt, 10, 64, 'sha512').toString('hex');
+}
+
+const verifyToken = (req, res, next) => { 
+    const authHeader = req.headers.authorization;
+    
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(403).json({ error: "Unauthorized" });
+    }
+
+    const token = authHeader.split(" ")[1]; // Extract the token part
+
+    try { 
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY); 
+        req.user = decoded; 
+        next(); // Proceed to the next middleware
+    } catch (err) { 
+        return res.status(401).json({ error: "Invalid token" }); 
+    } 
+}
 
 
 
@@ -67,14 +93,9 @@ async function listDatabases(client){
 module.exports = { 
     connectToDatabase, 
     ObjectId, 
-
-    /*getHighestId, 
-    createProperty, 
-    readProperties, 
-    updateProperty, 
-    deleteProperty */
+    hashPassword,
+    verifyToken,
 };
-
 
  connectToDatabase(listDatabases); 
 
